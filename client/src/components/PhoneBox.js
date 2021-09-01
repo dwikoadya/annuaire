027df543324b655ef4ad6b69128e52c9a2c10756
dwikoadya <dwikoadya@gmail.com>
2021-09-01 22:27:49 +0700

    Add Style

--- client/src/components/PhoneBox.js
+++ client/src/components/PhoneBox.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
-import ListContact from '../containers/ListContact'
+import ListContact from '../containers/ListContact';
 
 function PhoneBox() {
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.header}>Contacts</Text>
+      </View>
       <ListContact />
     </View>
-  )
+  );
 }
 
-export default PhoneBox
+const styles = StyleSheet.create({
+  container: {
+    flex: 5,
+    flexDirection: 'column',
+    backgroundColor: 'gainsboro',
+  },
+  box: {
+    // marginHorizontal: 20,
+    // backgroundColor: 'blue',
+  },
+  header: {
+    marginVertical: 20,
+    marginHorizontal: 10,
+    fontSize: 40,
+    // backgroundColor: 'green',
+  },
+});
+
+export default PhoneBox;
