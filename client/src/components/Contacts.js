1082f41e392d986fe3306148c3d8ae433acc1f16
dwikoadya <dwikoadya@gmail.com>
2021-09-01 22:27:02 +0700

    Add Style

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const Contact = (props) => {
+const Contact = props => {
   return (
-    <View>
-      <Text>{props.name}</Text>
-      <Text>{props.phone}</Text>
+    <View style={styles.item}>
+      <Image style={styles.avatar} source={{uri: props.avatar}}></Image>
+      <View style={styles.contact}>
+        <Text style={styles.name}>{props.name}</Text>
+        <Text style={styles.phone}>{props.phone}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 5,
+    marginVertical: 8,
+    marginHorizontal: 20,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    padding: 20,
+    borderRadius: 50,
+  },
+  name: {
+    fontSize: 20,
+  },
+  phone: {
+    fontSize: 10,
+  },
+  contact: {
+    flex: 4,
+    flexDirection: 'column',
+    marginHorizontal: 10,
+  },
+});
 
 export default Contact;
