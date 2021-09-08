776f30e6efddf513454b5b875f591b0c87da3ab2
dwikoadya <dwikoadya@gmail.com>
2021-09-08 22:47:59 +0700

    Add Dispatch Actions

--- client/src/containers/ContactActive.js
+++ client/src/containers/ContactActive.js
@@ -1,7 +1,17 @@
 import { connect } from 'react-redux'
 
+import { resendContact, deleteContact, onUpdateContact } from '../actions';
+
 import Contact from '../components/Contacts'
 
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onDelete: () => dispatch(deleteContact(ownProps.id)),
+  onResend: () => dispatch(resendContact(ownProps.id, ownProps.name, ownProps.phone)),
+
+  onUpdateContact: () => dispatch(onUpdateContact(ownProps.id))
+})
+
 export default connect(
-  null
+  null,
+  mapDispatchToProps
 )(Contact)
