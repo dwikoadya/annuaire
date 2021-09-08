04fb525a18af5dd265c42aaca7abc8abb77966f0
dwikoadya <dwikoadya@gmail.com>
2021-09-08 22:50:45 +0700

    Add Navigation to Detail Screen

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,15 +2,18 @@ import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from '../screens/Home';
-import AddContact from '../containers/AddContact';
+// import AddContact from '../containers/AddContact';
+import Detail from '../containers/Detail';
+import Add from '../screens/Add'
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Contact" component={Home} options={{ headerShown: false}} />
-      <Stack.Screen name="AddContact" component={AddContact} options={{ headerShown: false }} />
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false}} />
+      <Stack.Screen name="AddContact" component={Add} options={{ headerShown: false }} />
+      <Stack.Screen name="Details" component={Detail} options={{ headerShown: false }} />
     </Stack.Navigator>
   )
 }
